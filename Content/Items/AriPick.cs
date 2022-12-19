f0964a03a98075bbdba96250048dded6283262cd
Ari <45109939+Agamer12@users.noreply.github.com>
2022-12-18 23:38:32 -0800

    Added op Pickaxe

--- Content/Items/AriPick.cs
+++ Content/Items/AriPick.cs
@@ -1,31 +1,32 @@
-using System;
+﻿using System;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using Terraria.GameContent.Creative;
 
+using System;
 namespace CoolSword.Content.Items
 {
-	public class CryptBlade : ModItem
+	public class AriPick : ModItem
 	{
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Crypt Blade");
-            Tooltip.SetDefault("A crypt blade");
+            DisplayName.SetDefault("Ari's Pickaxe");
+            Tooltip.SetDefault("A pretty good pickaxe");
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
-        
+
         public override void SetDefaults()
         {
-            Item.width = 105;
-            Item.height = 75;
+            Item.width = 26;
+            Item.height = 26;
 
             Item.useStyle = ItemUseStyleID.Swing;
-            
-            Item.useTime = 20;
-            Item.useAnimation = 75;
 
-            Item.autoReuse = false;
+            Item.useTime = 1;
+            Item.useAnimation = 1;
+
+            Item.autoReuse = true;
 
             Item.DamageType = DamageClass.Melee;
             Item.damage = 500;
@@ -33,11 +34,13 @@ namespace CoolSword.Content.Items
 
             Item.crit = 5;
 
+            Item.pick = 9999;
+            Item.axe = 9999;
+
             Item.value = Item.buyPrice(gold: 50);
-            Item.rare = ItemRarityID.Pink;
+            Item.rare = ItemRarityID.Purple;
 
             Item.UseSound = SoundID.Item1;
-
         }
 
         public override void AddRecipes()
