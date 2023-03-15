a512306195c91c4fa7c6bff9840a1cbc61f4b5cd
Egamerk <112844189+Egamerk@users.noreply.github.com>
2023-03-15 00:24:41 -0700

    f

---
+++ Coolsword.cs
@@ -0,0 +1,9 @@
+using Terraria.ModLoader;
+
+namespace Coolsword
+{
+    public class Coolsword : Mod
+    {
+
+    }
+}
--- Vertex1.cs
+++
@@ -1,9 +0,0 @@
-using Terraria.ModLoader;
-
-namespace Vertex1
-{
-	public class Vertex1 : Mod
-	{
-
-	}
-}
