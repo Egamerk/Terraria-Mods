b53060000c98d5201dcb946fbf6e9b591fbd6fb0
Egamerk <112844189+Egamerk@users.noreply.github.com>
2023-03-15 00:28:46 -0700

    Update CryptBlade.cs

---
+++ Content/Items/CryptBlade.cs
@@ -0,0 +1,63 @@
+using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Terraria.DataStructures;
+
+
+namespace CoolSword.Content.Items
+{
+	public class CryptBlade : ModItem
+	{
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Crypt Blade");
+            Tooltip.SetDefault("A crypt blade");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+
+        }
+        
+        public override Vector2? HoldoutOffset()
+        {
+        return new Vector2(9000, 3000);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 105;
+            Item.height = 75;
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            
+            Item.useTime = 20;
+            Item.useAnimation = 75;
+
+            Item.autoReuse = false;
+
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 500;
+            Item.knockBack = 10f;
+
+            Item.crit = 5;
+
+            Item.value = Item.buyPrice(gold: 50);
+            Item.rare = ItemRarityID.Pink;
+
+            Item.UseSound = SoundID.Item1;
+
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.DirtBlock, 5)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+}
+
